f8d6a726c5cdb44983db7517d0e27dc35820f0a5
paul-b-b64 <belousko@inbox.ru>
2024-10-18 12:57:23 +0300

    14.1

---
+++ DB/Modul_14_1.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?)',
+                   (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', '1000'))
+
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = ? WHERE id = ?', (500, f'{i}'))
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE id = ?', (f'{i}',))
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
+
+results = cursor.fetchall()
+for user in results:
+    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+connection.commit()
+connection.close()
